25aa32707e5957d93abf6372f1168a1abff0a4df
Rafal Wilinski <raf.wilinski@gmail.com>
2024-12-11 14:18:43 +0100

    cal dot com

---
+++ src/components/CalDotCom.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import Cal, { getCalApi } from "@calcom/embed-react";
+import { useEffect } from "react";
+
+export default function CalDotCom() {
+	useEffect(() => {
+		console.log("CalDotCom");
+		(async function () {
+			const cal = await getCalApi({ namespace: "30min" });
+			cal("floatingButton", {
+				calLink: "rafal-wilinski-0fnqql/30min",
+				config: { layout: "month_view" },
+				buttonText: "Let's have a chat",
+			});
+			cal("ui", { hideEventTypeDetails: false, layout: "month_view" });
+		})();
+	}, []);
+
+	return <div id="cal-dot-com"></div>;
+}
