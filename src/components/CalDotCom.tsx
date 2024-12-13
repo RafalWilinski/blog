51176ce964c5e5cde5bf370e84ff35921b6061e5
Rafal Wilinski <raf.wilinski@gmail.com>
2024-12-13 09:06:20 +0100

    Refactor CalDotCom component: remove unused import and comment out its usage in Base layout

--- src/components/CalDotCom.tsx
+++ src/components/CalDotCom.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Cal, { getCalApi } from "@calcom/embed-react";
+import { getCalApi } from "@calcom/embed-react";
 import { useEffect } from "react";
 
 export default function CalDotCom() {
